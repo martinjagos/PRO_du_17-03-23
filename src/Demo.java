fe6a6c99a41cad9e530fd8adad0fde428f35f2a3
martinjagos <91561541+martinjagos@users.noreply.github.com>
2023-03-20 19:15:08 +0100

    Add files via upload

--- src/Demo.java
+++ src/Demo.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -23,21 +24,34 @@ public class Demo extends JFrame {
         menuBar.add(menu);
 
         JMenuItem openItem = new JMenuItem("Open");
+        openItem.setAccelerator(KeyStroke.getKeyStroke('O', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
         openItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 selectFileAndLoadContents();
             }
         });
+        menu.add(openItem);
+
         JMenuItem saveItem = new JMenuItem("Save");
+        saveItem.setAccelerator(KeyStroke.getKeyStroke('S', Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx()));
         saveItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 saveFileContents();
             }
         });
-        menu.add(openItem);
         menu.add(saveItem);
+
+        JMenuItem saveAsItem = new JMenuItem("Save As");
+        saveAsItem.setAccelerator(KeyStroke.getKeyStroke("control shift S"));
+        saveAsItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveAsFile();
+            }
+        });
+        menu.add(saveAsItem);
 
     }
 
@@ -54,20 +68,20 @@ public class Demo extends JFrame {
     private void loadFileContents(File selectedFile) {
         textAreaSource.setText("");
         aktualniSoubor = selectedFile;
+        boolean firstLine = true;
         try (
                 Scanner scanner =
                         new Scanner(new BufferedReader(
                                 new FileReader(selectedFile)))
         ) {
             while (scanner.hasNextLine()) {
-                int nuberOfRows = 0;
-                if (nuberOfRows == 0) {
+
+                if (firstLine == true){
                     textAreaSource.append(scanner.nextLine());
+                    firstLine = false;
                 }
-                textAreaSource.append("\n"+scanner.nextLine());
-
+                textAreaSource.append("\n" + scanner.nextLine());
             }
-
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -76,6 +90,22 @@ public class Demo extends JFrame {
     private void saveFileContents() {
         try (PrintWriter writer = new PrintWriter(
                 new BufferedWriter(new FileWriter(aktualniSoubor)))
+        ) {
+            writer.print(textAreaSource.getText());
+        } catch (IOException e) {
+            throw new RuntimeException(e.getLocalizedMessage());
+        }
+    }
+    private void saveAsFile(){
+        int result = chooser.showSaveDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = chooser.getSelectedFile();
+            saveFileAsContents(selectedFile);
+        }
+    }
+    private void saveFileAsContents(File soubor) {
+        try (PrintWriter writer = new PrintWriter(
+                new BufferedWriter(new FileWriter(soubor)))
         ) {
             writer.print(textAreaSource.getText());
         } catch (IOException e) {
